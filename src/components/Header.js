269a64d8f20e220a30717fbbe916cb16bc6096ca
gorkem <grkemarpaci@gmail.com>
2021-02-20 15:47:01 +0200

    json server added. before running npm start, run npm run server

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
 const Header = ({ title, onAdd, showAddTask }) => {
+  const location = useLocation();
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        color={!showAddTask ? "blue" : "red"}
-        text={!showAddTask ? "Add" : "Close"}
-        onClick={onAdd}
-      />
+      {location.pathname === "/" && (
+        <Button
+          color={!showAddTask ? "blue" : "red"}
+          text={!showAddTask ? "Add" : "Close"}
+          onClick={onAdd}
+        />
+      )}
     </header>
   );
 };
